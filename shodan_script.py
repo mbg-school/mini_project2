a743a368cb15d82c825c6080563007d8173a91ed
Luke B <laburgess97@gmail.com>
2021-10-08 17:58:48 -0400

    updated script

--- shodan_script.py
+++ shodan_script.py
@@ -1,44 +1,48 @@
 import shodan
 import requests
 import sys
+import json
 
-def getVulnerabilities():
-
+def getVulnerabilities(hostIP:str):
+    # returns a dictionary of the hosts with vulnerabilities
+    # { ip : [  ]    }
+    vulnerableHosts = {}
     SHODAN_API_KEY = "WvWmlWL28AKTwHZZDrDNMm1hbLuWOdCz"
     api = shodan.Shodan(SHODAN_API_KEY)
 
-    target = "www.packtpub.com"
-
-    dnsResolve = (
-        "https://api.shodan.io/dns/resolve?hostnames=" + target + "&key=" + SHODAN_API_KEY
-    )
-
     try:
-        # First we need to resolve our targets domain to an IP
-        resolved = requests.get(dnsResolve)
-        hostIP = resolved.json()[target]
-
         # Then we need to do a Shodan search on that IP
         host = api.host(hostIP)
-        print("IP: %s" % host["ip_str"])
-        print("Organization: %s" % host.get("org", "n/a"))
-        print("Operating System: %s" % host.get("os", "n/a"))
+        if host["vulns"]:
+            vulnerableHosts[hostIP] = {}
+            # Print vuln information
+            for item in host["vulns"]:
+                CVE = item.replace("!", "")
+                #print("Vulns: %s" % item)
+                exploits = api.exploits.search(CVE)
 
-        # Print all banners
-        for item in host["data"]:
-            print("Port: %s" % item["port"])
-            print("Banner: %s" % item["data"])
+                vulnerableHosts[hostIP][item] = exploits["matches"]
 
-        # Print vuln information
-        for item in host["vulns"]:
-            CVE = item.replace("!", "")
-            print("Vulns: %s" % item)
-            exploits = api.exploits.search(CVE)
-            for item in exploits["matches"]:
-                if item.get("cve")[0] == CVE:
-                    print(item.get("description"))
     except:
         "An error occured"
+
+    return(vulnerableHosts)
+
+def findVulnerabilities(host_file:str, dest_file:str):
+    with open(host_file, "r") as f:
+        rawJsonData = json.load(f)
+    
+    vulnerabilities = {}
+    # get hosts from json file
+    hosts = rawJsonData.keys()
+
+    for host in hosts:
+        vulnerabilities.update(getVulnerabilities(host))
+
+    print(json.dumps(vulnerabilities, indent=4))
+
+    with open(dest_file, "w") as f:
+        json.dump(vulnerabilities, f, indent=4)
 
 
 def getHosts(block:str, file:str):
@@ -57,49 +61,14 @@ def getHosts(block:str, file:str):
     # You can optionally pass in a number of results to be returned
     # each page and the number of pages you want returned.
 
-    # cursor = None
-
-    # while(True):
-    #         for page in h.search("IP: " + block, per_page=100, cursor=cursor):
-        
-    #             print(page)
-    #             # each page is a list of hosts ??
-    #             #hosts += page
-    #             for host in page:
-    #                 # each host is a dictionary object
-    #                 hosts[host['ip']] = host
-    #                 count += 1
-    #                 # hosts.append(host)
-    #                 # json.dumps
-    #                 #f.write(page)
-    #                 #f.write(page)
-                
-
-    #             cursor = page.links.next
-    #             if not cursor:
-    #                 break
-
     for page in h.search("IP: " + block, per_page=100, pages=27):
-        
-        #print(page)
-        # each page is a list of hosts ??
-        #hosts += page
         for host in page:
             # each host is a dictionary object
             hosts[host['ip']] = host
             count += 1
-            # hosts.append(host)
-            # json.dumps
-            #f.write(page)
-            #f.write(page)
+
     
     with open(file, 'w') as f:
         json.dump(hosts, f, indent=4)
         
-        
     print(count)
-
-    # # View each result returned
-    # # For `hosts` this looks like a mapping of IPs to view results
-    # query = h.search("service.service_name: HTTP", per_page=5, pages=2)
-    # print(query.view_all())
